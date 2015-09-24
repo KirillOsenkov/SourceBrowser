8e1699d403f38a5944d4afd45f34f5d09063bb93
Vladimir Matveev <vladima@microsoft.com>
2015-09-24 15:41:30 -0700

    run TypeScript analyzer with node

---
+++ src/HtmlGenerator/Pass1-Generation/TypeScriptSupport.AnalyzedFile.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.SourceBrowser.HtmlGenerator
+{
+    public class AnalyzedFile
+    {
+        public string fileName { get; set; }
+        public ClassifiedRange[] syntacticClassifications { get; set; }
+        public ClassifiedRange[] semanticClassifications { get; set; }
+        public string fileSymbolId { get; set; }
+    }
+}
---
+++ src/HtmlGenerator/Pass1-Generation/TypeScriptSupport.Hyperlink.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.SourceBrowser.HtmlGenerator
+{
+    public class Hyperlink
+    {
+        public string sourceFile { get; set; }
+        public int start { get; set; }
+        public string symbolId { get; set; }
+    }
+}
