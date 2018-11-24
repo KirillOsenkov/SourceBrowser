962457bdc0f778461f85ccff8438f9f2cc0a72b8
Kirill Osenkov <github@osenkov.com>
2018-11-23 21:01:04 -0800

    Add a CustomAppDomainInitializer so that MSBuildLocator is installed in all appdomains.
    We want MSBuildLocator assembly resolver to be installed in every appdomain created by the build, such as the one created by the ResolveCOMReference task.

---
+++ src/HtmlGenerator/AppDomainInitializer.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.SourceBrowser.HtmlGenerator
+{
+    public class AppDomainInitializer
+    {
+        /// <summary>
+        /// This is called everytime a new appdomain is created in the current process.
+        /// </summary>
+        public AppDomainInitializer()
+        {
+            Microsoft.Build.Locator.MSBuildLocator.RegisterDefaults();
+        }
+    }
+}
