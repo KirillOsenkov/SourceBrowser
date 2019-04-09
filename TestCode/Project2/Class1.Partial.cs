7e85e373c42b14cf11f7307b79b8dd8e0eca6209
Kirill Osenkov <github@osenkov.com>
2019-04-09 16:20:41 -0700

    Add a repro case for https://github.com/KirillOsenkov/SourceBrowser/issues/113

--- TestCode/Project2/Class1.Partial.cs
+++ TestCode/Project2/Class1.Partial.cs
@@ -18,3 +18,21 @@ class DerivedWord<T> : Word<T>
         Word<int> w = new DerivedWord<int>();
     }
 }
+
+//==========================================================
+// https://github.com/KirillOsenkov/SourceBrowser/issues/113
+//==========================================================
+
+public interface IAnimal
+{
+    void Eat();
+}
+
+public abstract class AbstractAnimal
+{
+    public void Eat() { }
+}
+
+public class Giraffe : AbstractAnimal, IAnimal
+{
+}
