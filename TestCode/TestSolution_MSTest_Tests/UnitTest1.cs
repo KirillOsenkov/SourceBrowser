24bd5dc740b97fe79805ae14fac38d8f1bdebb0f
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-07-08 12:14:31 +0100

    - MSTest sample test project

---
+++ TestCode/TestSolution_MSTest_Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolution_MSTest_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
