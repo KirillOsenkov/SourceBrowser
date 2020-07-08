201b51600f7900c3b8a39af9e09fdf1e5ad53921
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-07-08 12:22:26 +0100

    - generic (MSTest) sample project
    - EdgeDriver MSTest sample test project

---
+++ TestCode/TestSolution_UnitTest_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSolution_UnitTest_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSolution_UnitTest_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14ba174b-38f6-4342-bbdc-56ee3e8f3b79")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
