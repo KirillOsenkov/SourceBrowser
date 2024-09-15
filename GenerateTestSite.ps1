716bbea4b27aa98cdfa58b7b798c6d466d0a1a4b
Meir Blachman <meirblachman@gmail.com>
2024-09-15 18:50:48 +0300

    chore: add CI

--- GenerateTestSite.ps1
+++ GenerateTestSite.ps1
@@ -1,1 +1,8 @@
-src\HtmlGenerator\bin\Debug\net8.0\HtmlGenerator.exe TestCode\TestSolution.sln /out:src\HtmlGenerator\bin\Debug\net8.0\Web\Index
+param([switch]$force)
+
+$forceArg = ''
+if ($force) {
+    $forceArg = '/force'
+}
+
+src\HtmlGenerator\bin\Debug\net8.0\HtmlGenerator.exe TestCode\TestSolution.sln /out:src\HtmlGenerator\bin\Debug\net8.0\Web\Index $forceArg
